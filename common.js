1f3102673c7cf70daa0afcae2075a92f39a320ff
dogrizz <dogrizz@gmail.com>
2022-12-04 02:26:04 +0100

    Add link to VBakers company

--- common.js
+++ common.js
@@ -35,7 +35,8 @@ function FooterComponent() {
     view: function () {
       return m('footer', [
         m('span', 'Please '),
-        m('a', { target: '_blank', href: 'https://www.patreon.com/Joshua' }, 'support MF0 creator'),
+        m('a', { target: '_blank', href: 'https://www.patreon.com/Joshua' }, 'support one of'),
+        m('a', { target: '_blank', href: 'https://lumpley.games/' }, ' creators'),
         m('span', ' or '),
         m('a', { target: '_blank', href: 'https://glyphpress.com/talk/product/mobile-frame-zero-001-rapid-attack' }, 'buy a rulebook'),
         m('div', { class: 'disclaimer' }, [m('span', 'I am not the creator ;)')]),
